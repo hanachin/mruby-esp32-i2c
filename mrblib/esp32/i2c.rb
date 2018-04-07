94ff75829873aa98c711d5d3a11e5e0e15c45a54
Seiei Miyagi <hanachin@gmail.com>
2018-04-07 14:10:14 +0900

    Add ESP32::I2C#cmd

---
+++ mrblib/esp32/i2c.rb
@@ -0,0 +1,18 @@
+module ESP32
+  class I2C
+    # execute command
+    #
+    #     i2c.cmd do |cmd|
+    #       cmd.master_start
+    #       # ...
+    #       cmd.master_stop
+    #     end
+    def cmd(ticks_to_wait = ::ESP32::I2C::DEFAULT_TICKS_TO_WAIT)
+      cmd = ESP32::I2C::CmdHandle.new
+      yield cmd
+      master_cmd_begin(cmd, ticks_to_wait)
+    ensure
+      cmd.delete
+    end
+  end
+end
